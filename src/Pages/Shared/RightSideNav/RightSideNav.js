f4fdac8344d2d167e1593f65dffac6d605afd666
Avijit-Sarkar-Avi <avijit215054@gmail.com>
2022-10-22 12:46:12 +0600

    first commit

---
+++ src/Pages/Shared/RightSideNav/RightSideNav.js
@@ -0,0 +1,47 @@
+import { GoogleAuthProvider } from 'firebase/auth';
+import React, { useContext } from 'react';
+import Button from 'react-bootstrap/Button';
+import ButtonGroup from 'react-bootstrap/ButtonGroup';
+import ListGroup from 'react-bootstrap/ListGroup';
+import { FaGoogle, FaGithub, FaFacebook, FaYoutube, FaInstagram, FaTwitter, FaWhatsapp } from "react-icons/fa";
+import { AuthContext } from '../../../Context/AuthProvider/AuthProvider';
+import BrandCarousel from '../BrandCarousel/BrandCarousel';
+
+const RightSideNav = () => {
+
+    const { providerLogin } = useContext(AuthContext);
+
+    const googleProvider = new GoogleAuthProvider()
+
+    const handleGoogleSignIn = () => {
+        providerLogin(googleProvider)
+            .then(result => {
+                const user = result.user;
+                console.log(user);
+            })
+            .catch(error => console.error(error))
+    }
+
+    return (
+        <div>
+            <ButtonGroup vertical>
+                <Button onClick={handleGoogleSignIn} className='mb-2' variant="outline-primary"><FaGoogle></FaGoogle> LogIn with Google</Button>
+                <Button variant="outline-dark"><FaGithub></FaGithub> LogIn with GitHub</Button>
+            </ButtonGroup>
+            <div className='mt-4'>
+                <h5>Find us on</h5>
+                <ListGroup>
+                    <ListGroup.Item className='mb-2'><FaFacebook></FaFacebook> FaceBook</ListGroup.Item>
+                    <ListGroup.Item className='mb-2'><FaYoutube></FaYoutube> Youtube</ListGroup.Item>
+                    <ListGroup.Item className='mb-2'><FaInstagram></FaInstagram> Instergam</ListGroup.Item>
+                    <ListGroup.Item className='mb-2'><FaTwitter></FaTwitter> Twitter</ListGroup.Item>
+                    <ListGroup.Item className='mb-2'><FaWhatsapp></FaWhatsapp> What's App</ListGroup.Item>
+                </ListGroup>
+            </div>
+
+            <BrandCarousel></BrandCarousel>
+        </div>
+    );
+};
+
+export default RightSideNav;
---
+++ src/__steps.js
@@ -0,0 +1,15 @@
+/**
+ * BASIC PROJECT SETUP
+ 1. create firebase app
+ 2. get firebase configuration inside firebase.config.js 
+ 3. make sure to export app from firebase config
+ 4. create a react app
+ 5. install firebase and react-router-dom
+ 6. install react-bootstrap and copy css for import the index.js file
+ 7. 
+ */
+
+/**
+ * ROUTER SETUP
+ 1. 
+ */
